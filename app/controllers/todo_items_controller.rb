20781cf6e4ea2917f4b1d4668061b4e1bfac0063
pedromschmitt <ec2-user@ip-172-31-7-42.us-east-2.compute.internal>
2018-08-06 13:48:42 +0000

    Add mark as complete function, with initial styling

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,7 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
+  
     
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -14,12 +16,22 @@ class TodoItemsController < ApplicationController
       flash[:error] = "Todo item could not be deleted."
     end
     redirect_to @todo_list
-  end  
-    
+  end
+  
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed"
+  end
+  
+  
   private
   
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+  
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
   
   def todo_item_params
